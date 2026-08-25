bec1afacd5f2c74b8c747596ffc5e5a9f85f37cf
Ashishgargnotgonnaloose2248 <ashishgargofficial6548@gmail.com>
2026-08-26 00:26:53 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,32 @@
+class MinStack {
+public:
+
+    stack<int> st;
+    stack<int> minSt;
+
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(minSt.empty() || val<=minSt.top()){
+            minSt.push(val);
+        }
+        st.push(val);
+    }
+    
+    void pop() {
+        if(st.top() == minSt.top()){
+            minSt.pop();
+        }
+        st.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return minSt.top();
+    }
+};
